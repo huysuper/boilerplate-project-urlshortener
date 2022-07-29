0a88ed9ca061be3bd4774bf884a33f21875a9569
Huy Le <hoanghuyit98@gmail.com>
2022-07-29 14:59:04 +0000

    Implement API ShortUrl

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+let shorturlDB = [];
+let urlError = { error: 'invalid url' };
+
 
 app.use(cors());
 
@@ -14,10 +20,64 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var urlVerify = (req, res, next) => {
+  let url = req.body.url;
+  if (url == undefined) {
+    resstatus(400).json(urlError);
+    return;
+  }
+  let urlParser;
+  try {
+    urlParser = new URL(url);
+  } catch (error) {
+    res.status(400).json(urlError);
+    return;
+  }
+
+  const options = {
+    all:true
+  };
+  dns.lookup(urlParser.host, options, 
+    (err, address, family) => {
+        if (err) {
+          console.log(address, family);
+          res.status(400).json(urlError);
+        } else {
+          next();
+        }
+    });
+};
+app.post('/api/shorturl', urlencodedParser, urlVerify, function(req, res) {
+  let url = req.body.url;
+  if (shorturlDB.includes(url)) {
+    res.json({
+      orig
+  shorturlDB.push(url);
+  res.json({
+    original_url : url, 
+    short_url : shorturlDB.length
+  });
 });
+
+app.get('/api/shorturl/:short_url', function(req, res) {
+  let short_url = Number(req.params.short_url);
+  if (isNaN(short_url) || short_url < 1){
+    res.status(400).json({"error":"Wrong format"});
+    return;
+  }
+  short_url = short_url-1;
+  if (shorturlDB[short_url] == undefined) {
+    res.status(404).json({"error":"No short URL found for the given input"});
+    return;
+  }
+
+  res.redirect(302, shorturlDB[short_url]);
+});
+
+
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
